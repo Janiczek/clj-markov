a70094289f93b380009ceaa81509b147f3957db2
Martin Janiczek <martin@janiczek.cz>
2016-05-12 23:03:58 +0200

    Version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject janiczek/markov "0.3.0"
+(defproject janiczek/markov "0.3.1"
   :description "Markov chains in Clojure"
   :url "https://github.com/janiczek/markov"
   :license {:name "Eclipse Public License - v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]])
